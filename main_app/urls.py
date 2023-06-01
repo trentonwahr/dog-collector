6ec21656e0beeed950343359ed43694680d066f2
trentonwahr <trentwahr@gmail.com>
2023-06-01 16:45:31 -0400

    finish part 2

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('dogs/', views.dog_index, name='dog-index'),
+  path('dogs/<int:dog_id>/', views.dog_detail, name='dog-detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,28 +6,18 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-from django.http import HttpResponse
-
-class Dog:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Lolo', 'tabby', 'Kinda rude.', 3),
-  Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Dog('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+from .models import Dog
 
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def dog_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dog_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  return render(request, 'dogs/detail.html', { 'dog':dog })
